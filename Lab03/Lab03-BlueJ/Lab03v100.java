3c41f509b2107ac7b2e6adaaeabae518db5d87df
katemcg09 <kate.mc19@student.parishepiscopal.org>
2018-09-12 15:44:42 -0500

    added Lab03

--- Lab03/Lab03-BlueJ/Lab03v100.java
+++ Lab03/Lab03-BlueJ/Lab03v100.java
@@ -7,17 +7,17 @@ public class Lab03v100
 	public static void main(String[] args)
 	{
           System.out.println("Lab03, 100 Point Version\n");
-          int milliseconds = 10000123;
+          int milliseconds = 10000123; //finding hours, minutes, seconds, milliseconds from starting seconds
           int hours;
           int minutes;
           int seconds;
           System.out.println ("Starting milli-seconds:   " + milliseconds);
           hours = milliseconds / 3600000;
-          milliseconds = milliseconds % 3600000;
+          milliseconds = milliseconds % 3600000; //removing the hours from the intermediate value
           minutes = milliseconds / 60000;
-          milliseconds = milliseconds % 60000;
+          milliseconds = milliseconds % 60000; //removing the minutes from the intermediate value
           seconds = milliseconds / 1000;
-          milliseconds = milliseconds % 1000;
+          milliseconds = milliseconds % 1000; //removing the seconds from the final millisecond value
           System.out.println ("Hours:                    " + hours);
           System.out.println ("Minutes:                  " + minutes);
           System.out.println ("Seconds:                  " + seconds);
--- Lab03/Lab03-BlueJ/Lab03v80.java
+++ Lab03/Lab03-BlueJ/Lab03v80.java
@@ -7,14 +7,14 @@ public class Lab03v80
     public static void main(String[] args)
     {
       System.out.println("Lab03, 80 Point Version\n");
-      int seconds = 10000;
+      int seconds = 10000; //finding hours, minutes, seconds from starting seconds
       int hours;
       int minutes;
       System.out.println("Starting seconds:   " + seconds);
       hours = seconds / 3600;
-      seconds = seconds % 3600;
-      minutes = seconds / 60;
-      seconds = seconds % 60;
+      seconds = seconds % 3600; //removing the hours from the intermediate value
+      minutes = seconds / 60; 
+      seconds = seconds % 60; //removing the minutes from the final second value
       System.out.println("Hours:              " + hours);
       System.out.println("Minutes:            " + minutes);
       System.out.println("Seconds:            " + seconds);
