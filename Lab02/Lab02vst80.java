494247fe8a0673332f4f4b09a80e2887d3abe057
katemcg09 <kate.mc19@student.parishepiscopal.org>
2018-09-12 14:46:02 -0500

    added lab02

---
+++ Lab02/Lab02vst80.java
@@ -0,0 +1,21 @@
+  // Lab02v80.java
+  // The 80-point version of Lab 02 is an excercise in precisely copying a
+  // provided program/ followed by compiling the source code into bytecode
+  // and executing the bytecode file.
+  
+  
+  public class Lab02vst80
+  {
+    public static void main(String[] args)
+    {
+        System.out.println("Lab 02, 80 Point Version");
+        System.out.println();
+        System.out.println();
+        System.out.print("Measure today's knowledge ");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        
+    }
+    }  
